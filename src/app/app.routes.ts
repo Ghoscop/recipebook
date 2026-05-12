49b22c58ad04842823096b0fcbc74f065180586a
Ghoscop <heitormodanes@gmail.com>
2026-05-12 20:52:49 -0300

    git push -u origin master --forceInitial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import { RecipeList } from './pages/recipe-list/recipe-list';
+import { RecipeForm } from './pages/recipe-form/recipe-form';
+import { RecipeDetail } from './pages/recipe-detail/recipe-detail';
+
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: RecipeList,
+  },
+  {
+    path: 'nova',
+    component: RecipeForm,
+  },
+  {
+    path: 'receita/:id',
+    component: RecipeDetail,
+  },
+];
